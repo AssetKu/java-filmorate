eb2f572a01dbcdf9467e31898a7cc9ea2d5310cb
makeevas <ASET.MAKEEV@bcchub.kz>
2026-06-29 14:49:32 +0500

    HM_sprint_10_11

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.time.LocalDate;
 
@@ -74,9 +75,12 @@ public class FilmControllerTest {
         film.setReleaseDate(LocalDate.of(2000, 1, 1));
         film.setDuration(120);
 
+        film.setMpa(new Mpa(1, null));
+
         Film created = controller.create(film);
 
         Assertions.assertEquals(1, created.getId());
         Assertions.assertEquals("Film", created.getName());
+        Assertions.assertEquals(1, created.getMpa().getId());
     }
 }
